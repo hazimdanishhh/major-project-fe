1f4fbb5f946b672c12771cf9e18f4f545958afff
Hazim <hazimdanishhh@gmail.com>
2026-06-22 23:59:12 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# UNITAR International University Major Project Frontend Repository
---
+++ src/lib/reactQuery.js
@@ -0,0 +1,11 @@
+import { QueryClient } from "@tanstack/react-query";
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      keepPreviousData: true, // 🔥 important for pagination
+      staleTime: 1000 * 60 * 5, // 5 min cache
+      refetchOnWindowFocus: false,
+    },
+  },
+});
